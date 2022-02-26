d0bce00939677e40aa5e39990c01fa6eb0cce189
aureliapiekarska <aureliawpiekarska@gmail.com>
2022-02-26 21:41:57 +0100

    next

--- 02_variables/Homework_02.py
+++ 02_variables/Homework_02.py
@@ -1,10 +1,28 @@
 
 # zmodyfikuj skrypt tak, aby przyjmowal wartosci od uzytkownika
 distance = input('Podaj dlugosc trasy [km]')
-distance = int(distance)
+distance = float(distance)
 cost = input('Podaj koszt benzyny [PLN]')
-cost = int(cost)
-usag = input('Podaj spalanie na 100 [l]')
-usage = int(usag/100)
+cost = float(cost)
+us = input('Podaj spalanie na 100 [l]')
+us = float(us)
+usage = (us/100)
 result = (distance * cost * usage)
 print('Koszt wyprawy bedzie wynosi=', result)
+
+#Czy 43 - 13 będzie się równać 11 + 12 ?
+a = 43  - 13
+b = 11 + 12
+print (a == b)
+
+#Czy dzielenie 129 przez 17 bez reszty wynosi 3?
+c = 129 // 17
+print (c == 3)
+
+
+
+#Czy 247 podzielone przez 5 daje resztę 2?
+d = 247 % 5
+print (d == 2)
+
+
--- 02_variables/Zadanie1zostatniejgrupy.py
+++ 02_variables/Zadanie1zostatniejgrupy.py
@@ -1,5 +1,6 @@
-# stworz
-txt = "babababbaba"
+#stworz zmienna zadanie 1 z ostatniej grupy zadan
+
+txt = "babababbabaa"
 center_txt = len(txt) // 2
 prev_center = center_txt - 1 #3
 next_center = center_txt +1 #5
