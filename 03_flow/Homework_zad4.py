b4001e3602d7b234a92f860ed8f5e87a26c8b866
aureliapiekarska <aureliawpiekarska@gmail.com>
2022-03-01 22:44:05 +0100

    home

--- 03_flow/Homework_zad4.py
+++ 03_flow/Homework_zad4.py
@@ -1,6 +1,11 @@
-#zad4 if
+# zad4 if
 var_1 = 'dhfhasfb342'
-if len(var_1) < 5:
-    print ('zmienna dluzsza niz 5 'znakow'):
-#.......
-    print('zmienna zawiera litere a')
+b = 'a'
+if len(var_1) > 5:
+    print('zmienna dluzsza niz 5 znakow')
+if var_1.count('a'):
+    print('zmienna zwiera litere a')
+if var_1.count('a'):
+    var_1.split(',')
+print(var_1.replace('a','z'))
+
