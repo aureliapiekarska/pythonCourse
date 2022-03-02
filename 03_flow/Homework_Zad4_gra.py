5bb39296608d7fef98d257d6d79055660e4a7aa8
aureliapiekarska <aureliawpiekarska@gmail.com>
2022-03-03 00:05:29 +0100

    part

--- 03_flow/Homework_Zad4_gra.py
+++ 03_flow/Homework_Zad4_gra.py
@@ -5,17 +5,28 @@ figure = input('Papier, kamien, nozyce, podaj jedna figure:')
 com = (random.choice(opt))
 print(com)
 
+score_p = 0
+score_c = 0
+
 if (figure == 'papier') and (com == 'nozyce'):
+    score_c = score_c + 1
     print('wygyrwa komp')
-if (figure == 'nozyce') and (com == 'papier'):
+elif (figure == 'nozyce') and (com == 'papier'):
+    score_p = score_p + 1
     print('wygyrwasz')
-if (figure == 'papier') and (com == 'kamien'):
+elif (figure == 'papier') and (com == 'kamien'):
+    score_p = score_p + 1
     print('wygrywasz')
-if (figure == 'kamien') and (com == 'papier'):
+elif (figure == 'kamien') and (com == 'papier'):
+    score_c = score_c + 1
     print('wygrywa komp')
-if (figure == 'kamien') and (com == 'nozyce'):
+elif (figure == 'kamien') and (com == 'nozyce'):
+    score_p = score_p + 1
     print('wygrywasz')
-if (figure == 'nozyce') and (com == 'kamien'):
+elif (figure == 'nozyce') and (com == 'kamien'):
+    score_c = score_c + 1
     print('wygrywa komp')
 else:
     print('remis')
+
+print('Masz:', score_p, 'Komputer:', score_c)
