d3c9e2d5bca301a92fe4e8ba97d1fafc4db8c1d7
aureliapiekarska <aureliawpiekarska@gmail.com>
2022-03-12 09:59:11 +0100

    new

---
+++ 04_collection/Homework_Zad8.py
@@ -0,0 +1,11 @@
+p1=[('Poland','Jane'),('Poland','Sophie'), ('Poland','Catherine')]
+h1= [('Holand','Jane'),('Holand','Eve'), ('Holand','Madeline')]
+po1= [('Portugal','Jane'),('Portugal','Maria'), ('Portugal','Ann')]
+g1= [('Germany','Jane'),('Germany','Yulia'), ('Portugal','Ann')]
+d1= [('Denmark','Jane'),('Denmark','Yulia'), ('Denmark','Sophie')]
+
+sum_name = p1+h1+po1+g1+d1
+print(sum_name)
+translate = dict(sum_name)
+print(translate)
+
