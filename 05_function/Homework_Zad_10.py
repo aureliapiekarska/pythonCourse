ac128bfd201c23d219eb774277558632bbd4ec0a
aureliapiekarska <aureliawpiekarska@gmail.com>
2022-03-27 17:38:19 +0200

    new

--- 05_function/Homework_Zad_10.py
+++ 05_function/Homework_Zad_10.py
@@ -1,21 +1,21 @@
-#wisilece z hackatonu
 word_guess = 'slava'
 counter = len(word_guess)
-user_guess = []
-turns = 6
-used_letters = ''
-
-
 gamer = input('Jak masz na imię drogi graczu? -> ')
 print('Witam w grze wisielec', gamer)
 print('Haslo sklada sie z ', counter, 'liter.')
-print('Masz', 6, 'prob, aby odgadnac haslo')
+turns = 6
+print('Masz', turns, 'prob, aby odgadnac haslo')
+
+def game(turns):
 
 
-def turnin(turns):
+    user_guess = []
     for i in range(counter):
         user_guess.append('_')
+
     print(user_guess)
+
+    used_letters = ''
 
     while turns > 0:
         to_show = '_'.join(user_guess)
@@ -23,21 +23,23 @@ def turnin(turns):
         guess = input('Podaj litere,lub cale haslo jesli juz je znasz:')
         guess = guess.lower()
 
-    if guess in word_guess:
-        for index in range(counter):
-            if word_guess[index] == guess:
-                user_guess[index] == guess
-                break
-    if guess == word_guess:
-        print('Wygrana! to jest to słowo:', guess)
-    else:
-        print('Nie ma tej litery w słowie')
-        turns -= 1
-        if turns == 0:
-            print('Koniec gry')
+        if guess == word_guess:
+            print('Wygrana! to jest to słowo:', guess)
+            break
 
+        if guess in word_guess:
+            for index in range(counter):
+                if word_guess[index] == guess:
+                    user_guess[index] = guess
+
+        else:
+            print('Nie ma tej litery w słowie')
+            turns -= 1
+            if turns == 0:
+                print('Koniec gry')
+            break
 
         print('Zostało Ci prób ->', turns)
         print('Podaj kolejna litere')
 
-turnin(turns)
+game(turns)
