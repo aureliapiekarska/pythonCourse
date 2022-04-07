5650565367e11445af5ffc286dddb5ea77f193b6
aureliapiekarska <aureliawpiekarska@gmail.com>
2022-04-07 16:36:35 +0200

    n

--- 09_algor/Homework_Zad2.py
+++ 09_algor/Homework_Zad2.py
@@ -1,16 +1,23 @@
+def number(i):
 
-def main(n):
-    number_list = []
-    x = 1
-    while x < n:
-        if x % 5 == 0:
-            number_list.append(x)
-        elif x % 7 == 0:
-            number_list.append(x)
-        x = x + 1
-    print(number_list)
+    empty_list = []
+
+    j = 1
+
+    while j < i:
+
+        if j % 5 == 0:
+
+            empty_list.append(j)
+
+        elif j % 7 == 0:
+
+            empty_list.append(j)
+        j = j + 1
+
+    print(empty_list)
 
 
 if __name__ == '__main__':
-    n = int(input('Podaj liczbe: '))
-    main(n)
+    i = int(input('Please provide number: '))
+    number(i)
