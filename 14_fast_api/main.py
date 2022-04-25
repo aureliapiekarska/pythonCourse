c172afebe362932b6e97e2799369933b0f21bd59
aureliapiekarska <aureliawpiekarska@gmail.com>
2022-04-25 19:44:57 +0200

    api

---
+++ 14_fast_api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+app.counter = 0
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+def hello_name(name: str):
+    return f"Hi {name.capitalize()}"
+
+@app.get("/counter")
+def count():
+    app.counter = +1
+    return{"counter": app.counter}
+
+
